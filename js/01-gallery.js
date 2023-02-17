a191982f9adcc471dff8e5da284960a4c2a0b52c
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2023-02-17 17:47:06 +0200

    first-home-work-solution

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+
+function createGallaryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join("");
+}
+
+const Markup = createGallaryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", Markup);
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  event.preventDefault();
+
+  const closeModal = (event) => {
+    const Escape = "Escape";
+
+    if (event.code === Escape) {
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `
+      <img src="${event.target.dataset.source}" width="800" height="600">
+    `,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeModal);
+      },
+
+      onclose: () => {
+        window.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+  instance.show();
+}
 
 console.log(galleryItems);
