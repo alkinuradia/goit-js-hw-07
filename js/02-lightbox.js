695b34a8495510530baeda87ceddb2e57c478a82
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2023-02-18 11:15:30 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
-const items = [];
-galleryItems.forEach((element) => {
-    const galleryLink = document.createElement("a");
-    galleryLink.classList.add("gallery__link");
-    galleryLink.href = element.original;
 
-    const galleryImage = document.createElement("img");
-    galleryImage.classList.add("gallery__image");
-    galleryImage.src = element.preview;
-    galleryImage.setAttribute("title", element.description);
-    galleryImage.alt = element.description;
-    galleryLink.append(galleryImage);
-    items.push(galleryLink);
-});
+function createGallaryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>`;
+    })
+    .join("");
+}
 
-gallery.append(...items);
+const Markup = createGallaryMarkup(galleryItems);
 
-new SimpleLightbox(".gallery a", {
+gallery.insertAdjacentHTML("beforeend", Markup);
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
     captionDelay: 250,
 });
 
